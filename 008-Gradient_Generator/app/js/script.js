cf9189d3aa0d371c62acdd0abd291e7a5ef58ba2
thisiskushal31 <guptakushal131@gmail.com>
2022-08-02 21:43:30 +0530

    Final Code Changes

--- 008-Gradient_Generator/app/js/script.js
+++ 008-Gradient_Generator/app/js/script.js
@@ -1,25 +1,21 @@
-let colorOne = document.getElementById('color-a');
-let colorTwo = document.getElementById('color-b');
-let currentDirection = 'to bottom';
+let colorOne = document.getElementById('colorSelection-a');
+let colorTwo = document.getElementById('colorSelection-b');
+let currentDirection = 'to right';
 let outputCode = document.getElementById('code');
 
 function setDirection(value, _this) {
-    let directions = document.querySelectorAll(".buttons button");
-    // console.log(directions);
+    let directions = document.querySelectorAll(".arrowButtons button");
     for (let i of directions) {
         i.classList.remove('active');
     }
     _this.classList.add('active');
     currentDirection = value;
-    // console.log(currentDirection);
 }
+
 function generateCode() {
     outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value});`
-
-
     document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
 }
-
 
 function copyText() {
     outputCode.select();
