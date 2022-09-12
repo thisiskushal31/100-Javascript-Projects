2de65921fb2ddbf831402dd07f1f8dabc9505726
thisiskushal31 <guptakushal131@gmail.com>
2022-09-12 22:53:33 +0530

    Script Updated

--- 011-Image_Downloader/app/js/script.js
+++ 011-Image_Downloader/app/js/script.js
@@ -1,1 +1,15 @@
-// Add Your JavaScript Here
+// download image from html element
+
+const media = document.querySelector(" . media ");
+const input = document.querySelector(" input ");
+const button = document.querySelector(" button ");
+
+input.addEventListener("input", e => {
+    e.preventDefault();
+    let url = e.target.value;
+
+    if (!url.trim()) return;
+
+    fetchImage(url);
+})
+
